9cb2cdd143442e264ce98ddd77dc901977c9289e
Alessandro <17389877+alezaffa@users.noreply.github.com>
2021-09-19 16:06:53 +0200

    Refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from '@/App'
+import router from '@/router'
 import FlagIcon from 'vue-flag-icon'
-import 'vue-awesome/icons'
-
 import Icon from 'vue-awesome/components/Icon'
 import VmBackTop from 'vue-multiple-back-top'
+import 'vue-awesome/icons'
 
-Vue.component(VmBackTop.name, VmBackTop)
-
-// globally (in your main .js file)
-Vue.component('icon', Icon)
-
+Vue.component(VmBackTop.name, VmBackTop, Icon)
 Vue.use(FlagIcon)
-
 Vue.config.productionTip = false
+
 require('../node_modules/bootstrap/less/bootstrap.less')
 
 /* eslint-disable no-new */
